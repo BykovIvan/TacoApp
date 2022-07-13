5795398e4ee7b1bb1dca8cbb875275d7123bdd3a
Ivan <bukov123@yandex.ru>
2022-07-13 16:56:04 +0300

    feat: добавлены реализации для БД

---
+++ src/main/java/ru/bykov/hellospring/data/IngredientRepository.java
@@ -0,0 +1,11 @@
+package ru.bykov.hellospring.data;
+
+import ru.bykov.hellospring.model.Ingredient;
+
+import java.util.Optional;
+
+public interface IngredientRepository {
+    Iterable<Ingredient> findAll();
+    Optional<Ingredient> findById(String id);
+    Ingredient save(Ingredient ingredient);
+}
